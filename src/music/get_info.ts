f0a25c7fcbdfd77a2223d66cf7a3d02fa224b073
abulleDev <olives2739@gmail.com>
2024-07-13 01:07:02 +0900

    refactor: Refine VideoInfo interface

--- src/music/get_info.ts
+++ src/music/get_info.ts
@@ -1,19 +1,22 @@
 import { spawn } from 'child_process';
 
-interface Info {
+interface VideoInfo {
+  id?: string;
   title: string;
-  uploader: string | undefined;
+  description?: string;
+  uploader?: string;
   webpage_url: string;
-  uploader_url: string | undefined;
-  thumbnail: string | undefined;
+  uploader_url?: string;
+  thumbnail?: string;
   extractor: string;
+  [key: string]: any;
 }
 
-export default function getInfo(url: string): Promise<Info> {
+export default function getInfo(url: string): Promise<VideoInfo> {
   const ytdlp = spawn('yt-dlp', ['--no-playlist', '-J', url]);
   return new Promise((resolve, reject) => {
     ytdlp.stdout.once('data', async (json) => {
-      const info: Info | null = JSON.parse(json);
+      const info: VideoInfo | null = JSON.parse(json);
 
       // Check url validity
       if (info === null) reject(new Error('Invalid URL.'));
