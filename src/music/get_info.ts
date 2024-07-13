4116fbfe51014b41f9966e8c2b4fa3006e1933a6
abulleDev <olives2739@gmail.com>
2024-07-13 16:59:34 +0900

    fix: Issue with handling large JSON output on Linux

--- src/music/get_info.ts
+++ src/music/get_info.ts
@@ -18,10 +18,14 @@ interface SearchInfo {
 }
 
 export function getInfo(url: string): Promise<VideoInfo> {
+  let jsonString = '';
   const ytdlp = spawn('yt-dlp', ['--no-playlist', '-J', url]);
+  ytdlp.stdout.on('data', (data) => {
+    jsonString += data;
+  });
   return new Promise((resolve, reject) => {
-    ytdlp.stdout.once('data', (json) => {
-      const info: VideoInfo | null = JSON.parse(json);
+    ytdlp.stdout.once('close', () => {
+      const info: VideoInfo | null = JSON.parse(jsonString);
 
       // Check url validity
       if (info === null) reject(new Error('Invalid URL.'));
@@ -31,6 +35,7 @@ export function getInfo(url: string): Promise<VideoInfo> {
 }
 
 export function getSearchInfo(music: string): Promise<VideoInfo> {
+  let jsonString = '';
   const ytdlp = spawn('yt-dlp', [
     '--no-playlist',
     '-J',
@@ -38,9 +43,12 @@ export function getSearchInfo(music: string): Promise<VideoInfo> {
     'ytsearch',
     music,
   ]);
+  ytdlp.stdout.on('data', (data) => {
+    jsonString += data;
+  });
   return new Promise((resolve, reject) => {
-    ytdlp.stdout.once('data', (json) => {
-      const searchInfo: SearchInfo | null = JSON.parse(json);
+    ytdlp.stdout.once('close', () => {
+      const searchInfo: SearchInfo | null = JSON.parse(jsonString);
 
       // Check search results existence
       if (searchInfo === null) reject(new Error('No search results found.'));
